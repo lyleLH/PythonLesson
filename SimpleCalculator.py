b7abf24290a246fca3281b3f1552af831ce1cb08
lyleKP <v2top1@163.com>
2016-12-06 16:19:50 +0800

    增加交互提示

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,3 +1,3 @@
 def foo(op,n1,n2):
 	return eval("%d %s %d" %(n1,op,n2))
-print foo(raw_input (),int(raw_input()),int(raw_input()))
+print  'result is : %s \n' % foo(raw_input('input an operater:\n'),int(raw_input('input an int:\n')),int(raw_input('input another int:\n')))
